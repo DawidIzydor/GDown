92d297f9d9e5e994e9f96d690f1239965d05f4a5
DawidIzydor <admin@dawid-izydor.pl>
2018-10-29 21:12:19 +0100

    CBR manager

---
+++ gdownload/CBRManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gdownload
+{
+    class CBRManager
+    {
+
+        private bool cbrlocked = false;
+
+        public bool CreateCbr(string frompath, string savepath)
+        {
+            if (cbrlocked) return false;
+
+            cbrlocked = true;
+
+            try
+            {
+
+                if (File.Exists(savepath) == true)
+                {
+                    File.Delete(savepath);
+                }
+
+                ZipFile.CreateFromDirectory(frompath, savepath);
+            }
+            catch (Exception)
+            {
+                cbrlocked = false;
+                return false;
+            }
+
+            cbrlocked = false;
+            return true;
+        }
+
+    }
+}
